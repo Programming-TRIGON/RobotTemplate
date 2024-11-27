615e15d4352210858e638d45243bda67eb3f7055
Strflightmight09 <148347057+Strflightmight09@users.noreply.github.com>
2024-11-27 15:56:29 +0200

    Update FieldConstants.java

--- src/main/java/frc/trigon/robot/constants/FieldConstants.java
+++ src/main/java/frc/trigon/robot/constants/FieldConstants.java
@@ -11,25 +11,24 @@ import java.io.IOException;
 import java.util.HashMap;
 
 public class FieldConstants {
+    private static final boolean SHOULD_USE_HOME_TAG_LAYOUT = false;
+    public static final AprilTagFieldLayout APRIL_TAG_FIELD_LAYOUT = createAprilTagFieldLayout();
+    private static final Transform3d TAG_OFFSET = new Transform3d(0, 0, 0, new Rotation3d(0, 0, 0));
+
+    public static final HashMap<Integer, Pose3d> TAG_ID_TO_POSE = fieldLayoutToTagIdToPoseMap();
     public static final double
             FIELD_LENGTH_METERS = 16.54175,
-            FIELD_WIDTH_METERS = 8.02;
+            FIELD_WIDTH_METERS = 8.21;
 
-    private static final boolean SHOULD_USE_HOME_TAG_LAYOUT = false;
-    private static final Transform3d TAG_OFFSET = new Transform3d(0, 0, 0, new Rotation3d(0, 0, 0));
-    public static final AprilTagFieldLayout APRIL_TAG_FIELD_LAYOUT;
-
-    static {
+    private static AprilTagFieldLayout createAprilTagFieldLayout() {
         try {
-            APRIL_TAG_FIELD_LAYOUT = SHOULD_USE_HOME_TAG_LAYOUT ?
-                    AprilTagFieldLayout.loadFromResource("path/to/layout.json") :
-                    AprilTagFields.kDefaultField.loadAprilTagLayoutField();//TODO:Switch for year
+            return SHOULD_USE_HOME_TAG_LAYOUT ?
+                    AprilTagFieldLayout.loadFromResource("path/to/home/layout.json") :
+                    AprilTagFields.kDefaultField.loadAprilTagLayoutField();//TODO: Change for year
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-
-    public static final HashMap<Integer, Pose3d> TAG_ID_TO_POSE = fieldLayoutToTagIdToPoseMap();
 
     private static HashMap<Integer, Pose3d> fieldLayoutToTagIdToPoseMap() {
         final HashMap<Integer, Pose3d> tagIdToPose = new HashMap<>();
