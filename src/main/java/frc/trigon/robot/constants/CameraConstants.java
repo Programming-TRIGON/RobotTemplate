476a27bce2040a2adbb7c6423b2472cc6fde6938
Strflightmight09 <148347057+Strflightmight09@users.noreply.github.com>
2024-12-15 11:34:48 +0200

    Update CameraConstants.java

--- src/main/java/frc/trigon/robot/constants/CameraConstants.java
+++ src/main/java/frc/trigon/robot/constants/CameraConstants.java
@@ -6,6 +6,6 @@ import frc.trigon.robot.poseestimation.t265.T265;
 public class CameraConstants {
     public static final T265 T265 = new T265();
 
-    private static final PoseEstimatorConstants.StandardDeviations CAMERA_STANDARD_DEVIATIONS = new PoseEstimatorConstants.StandardDeviations(0.02, 0.02);
+    private static final PoseEstimatorConstants.StandardDeviations STANDARD_DEVIATIONS = new PoseEstimatorConstants.StandardDeviations(0.02, 0.02);
     //TODO: implement CameraConstants
 }
