667089184810f6b6c8d778b3e24e74ce6b744c19
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2024-12-28 20:07:50 +0200

    Added example path, not working though

--- src/main/java/frc/trigon/robot/constants/PathPlannerConstants.java
+++ src/main/java/frc/trigon/robot/constants/PathPlannerConstants.java
@@ -1,12 +1,14 @@
 package frc.trigon.robot.constants;
 
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
 import com.pathplanner.lib.commands.PathfindingCommand;
 import com.pathplanner.lib.config.PIDConstants;
 import com.pathplanner.lib.config.RobotConfig;
 import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 import com.pathplanner.lib.path.PathConstraints;
 import com.pathplanner.lib.pathfinding.Pathfinding;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
 import frc.trigon.robot.RobotContainer;
 import org.json.simple.parser.ParseException;
 import org.trigon.hardware.RobotHardwareStats;
@@ -47,8 +49,7 @@ public class PathPlannerConstants {
     private static void configureAutoBuilder() {
         AutoBuilder.configure(
                 RobotContainer.POSE_ESTIMATOR::getCurrentEstimatedPose,
-                (pose) -> {
-                },
+                RobotContainer.POSE_ESTIMATOR::resetPose,
                 RobotContainer.SWERVE::getSelfRelativeVelocity,
                 RobotContainer.SWERVE::selfRelativeFeedForwardDrive,
                 AUTO_PATH_FOLLOWING_CONTROLLER,
@@ -67,6 +68,9 @@ public class PathPlannerConstants {
     }
 
     private static void registerCommands() {
-//        NamedCommands.registerCommand(name, command);//TODO:Implement NamedCommands
+        NamedCommands.registerCommand("PrepareForShooting", new PrintCommand("PrepareForShooting"));
+        NamedCommands.registerCommand("Collect", new PrintCommand("Collecting"));
+        NamedCommands.registerCommand("FeedNote", new PrintCommand("Feeding"));
+        //TODO:Implement NamedCommands
     }
 }
