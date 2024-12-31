24d893a8c03903200d6ec7d93ca3876206d36e6e
Strflightmight09 <148347057+Strflightmight09@users.noreply.github.com>
2024-12-31 19:17:39 +0200

    Update RelativeRobotPoseSourceConstants.java

--- src/main/java/frc/trigon/robot/poseestimation/relativerobotposesource/RelativeRobotPoseSourceConstants.java
+++ src/main/java/frc/trigon/robot/poseestimation/relativerobotposesource/RelativeRobotPoseSourceConstants.java
@@ -1,9 +1,9 @@
 package frc.trigon.robot.poseestimation.relativerobotposesource;
 
-import frc.trigon.robot.poseestimation.poseestimator.PoseEstimatorConstants;
+import frc.trigon.robot.poseestimation.poseestimator.StandardDeviations;
 
 public class RelativeRobotPoseSourceConstants {
-    public static final PoseEstimatorConstants.StandardDeviations STANDARD_DEVIATIONS = new PoseEstimatorConstants.StandardDeviations(0.0000001, 0.0000001);
+    public static final StandardDeviations STANDARD_DEVIATIONS = new StandardDeviations(0.0000001, 0.0000001);
 
     static final double
             MAXIMUM_TRANSLATION_VELOCITY_FOR_OFFSET_RESETTING_METERS_PER_SECOND = 0,
