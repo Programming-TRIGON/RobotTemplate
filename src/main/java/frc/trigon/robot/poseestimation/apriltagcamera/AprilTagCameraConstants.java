28b2f13b48c156c2bc9d5559f5e056047ed34366
Yishai Levy <96019039+levyishai@users.noreply.github.com>
2025-01-08 22:19:57 +0200

    Extra newline

--- src/main/java/frc/trigon/robot/poseestimation/apriltagcamera/AprilTagCameraConstants.java
+++ src/main/java/frc/trigon/robot/poseestimation/apriltagcamera/AprilTagCameraConstants.java
@@ -15,7 +15,6 @@ import java.util.function.Function;
 public class AprilTagCameraConstants {
     static final double MAXIMUM_DISTANCE_FROM_TAG_FOR_ACCURATE_SOLVE_PNP_RESULT_METERS = 2;
     static final Pose2d[] EMPTY_POSE_LIST = new Pose2d[0];
-
     static final double MAXIMUM_AMBIGUITY = 0.4;
 
     public static final VisionSystemSim VISION_SIMULATION = new VisionSystemSim("VisionSimulation");
