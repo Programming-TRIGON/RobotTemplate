7cc10e94e97999a1fa5ebec8bf3682703e5b790a
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2025-12-22 22:23:02 +0200

    temp

---
+++ src/main/java/frc/trigon/lib/trigonlib/hardware/misc/simplesensor/io/AnalogSensorIO.java
@@ -0,0 +1,17 @@
+package frc.trigon.lib.trigonlib.hardware.misc.simplesensor.io;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+import frc.trigon.lib.trigonlib.hardware.misc.simplesensor.SimpleSensorIO;
+import org.trigon.hardware.misc.simplesensor.SimpleSensorInputsAutoLogged;
+
+public class AnalogSensorIO extends SimpleSensorIO {
+    private final AnalogInput analogInput;
+
+    public AnalogSensorIO(int channel) {
+        analogInput = new AnalogInput(channel);
+    }
+
+    public void updateInputs(SimpleSensorInputsAutoLogged inputs) {
+        inputs.value = analogInput.getValue();
+    }
+}
