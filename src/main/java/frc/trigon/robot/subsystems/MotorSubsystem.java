7626471430c40879f89dbcbe208e930bebfc9243
Strflightmight09 <148347057+Strflightmight09@users.noreply.github.com>
2026-01-12 23:57:25 +0200

    Fix build config stuff and new wpilib units dir

--- src/main/java/frc/trigon/robot/subsystems/MotorSubsystem.java
+++ src/main/java/frc/trigon/robot/subsystems/MotorSubsystem.java
@@ -1,15 +1,14 @@
 package frc.trigon.robot.subsystems;
 
-import edu.wpi.first.units.measure.Units;
+import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.sysid.SysIdRoutineLog;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
-import frc.trigon.robot.RobotContainer;
-import org.littletonrobotics.junction.networktables.LoggedNetworkBoolean;
 import lib.hardware.RobotHardwareStats;
+import org.littletonrobotics.junction.networktables.LoggedNetworkBoolean;
 
 import java.util.ArrayList;
 import java.util.List;
