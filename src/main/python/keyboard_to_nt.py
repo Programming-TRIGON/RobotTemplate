babc6163b84b3593593a21a056805f1834d8636e
noamgreen12 <noamarielgreen@gmail.com>
2026-01-13 01:48:46 +0200

    Update keyboard_to_nt.py

--- src/main/python/keyboard_to_nt.py
+++ src/main/python/keyboard_to_nt.py
@@ -1,8 +1,16 @@
 import ntcore
+import keyboard
 import time
-from pynput import keyboard
 
 def main():
+    def on_action(event: keyboard.KeyboardEvent):
+        if event.name == "/":
+            return
+        if event.is_keypad:
+            table.putBoolean("numpad"+event.name, event.event_type == keyboard.KEY_DOWN)
+        else:
+            table.putBoolean(event.name.lower(), event.event_type == keyboard.KEY_DOWN)
+
     ntcoreinst = ntcore.NetworkTableInstance.getDefault()
 
     print("Setting up NetworkTables client")
@@ -10,6 +18,7 @@ def main():
     ntcoreinst.setServer("127.0.0.1")
     ntcoreinst.startDSClient()
 
+    # Wait for connection
     print("Waiting for connection to NetworkTables server...")
     while not ntcoreinst.isConnected():
         time.sleep(0.1)
@@ -17,27 +26,8 @@ def main():
     print("Connected!")
     table = ntcoreinst.getTable("SmartDashboard/keyboard")
 
-    def on_press(key):
-        handle_key(key, True)
+    keyboard.hook(lambda e: on_action(e))
+    keyboard.wait()
 
-    def on_release(key):
-        handle_key(key, False)
-
-    def handle_key(key, is_down):
-        try:
-            # Character keys
-            if key.char == "/":
-                return
-            table.putBoolean(key.char.lower(), is_down)
-        except AttributeError:
-            # Special keys (shift, space, arrows, etc.)
-            table.putBoolean(str(key).replace("Key.", ""), is_down)
-
-    with keyboard.Listener(
-        on_press=on_press,
-        on_release=on_release
-    ) as listener:
-        listener.join()
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
