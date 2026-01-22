346599be30a9be99aa7a434ba83b71286d77411d
ShmayaR <shmaya.rosenblatt@gmail.com>
2026-01-22 17:34:30 +0200

    Update RobotConstants.java

--- src/main/java/frc/trigon/robot/constants/RobotConstants.java
+++ src/main/java/frc/trigon/robot/constants/RobotConstants.java
@@ -5,7 +5,7 @@ import frc.trigon.lib.utilities.FilesHandler;
 import frc.trigon.robot.Robot;
 
 public class RobotConstants {
-    public static final boolean USE_CANIVORE = true;
+    public static final boolean USE_CANIVORE = RobotHardwareStats.isSimulation() ? true : false;
     public static final String CANIVORE_NAME = USE_CANIVORE ? "SwerveCANivore" : "";
     public static final String LOGGING_PATH = Robot.IS_REAL ? "/media/sda1/akitlogs/" : FilesHandler.DEPLOY_PATH + "logs/";
     private static final RobotHardwareStats.ReplayType REPLAY_TYPE = RobotHardwareStats.ReplayType.NONE;
